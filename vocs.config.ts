4d066514049d4b08382e6d42363ada2163cc69a6
earth2travis <travis.mccutcheon@gmail.com>
2024-06-11 19:12:03 -0500

    Scaffold Vocs project

---
+++ vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vocs';
+
+export default defineConfig({
+  title: 'WarCouncil',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'Example',
+      link: '/example',
+    },
+  ],
+});
